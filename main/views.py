015cf16c97a863fcfbcccaf47c5194a937a5b839
Sohan <abc@gmail.com>
2025-07-12 18:18:18 +0545

    Fixed name error

--- main/views.py
+++ main/views.py
@@ -4,13 +4,16 @@ from django.utils import timezone
 
 # Home page: school info, news bar, latest posts
 
+def get_news():
+    return News.objects.filter(is_active=True).order_by('-date')[:5]
+
 def home(request):
     school_info = {
-        'name': 'Balabhadra Janat Higher Secondary School',
+        'name': 'Balabhadra Janata Higher Secondary School',
         'address': 'Mechinagar-11, Dhaijan, Jhapa',
         'phone': '023460046',
     }
-    news = News.objects.filter(is_active=True).order_by('-date')[:5]
+    news = get_news()
     latest_posts = Post.objects.order_by('-date')[:3]
     return render(request, 'main/home.html', {
         'school_info': school_info,
@@ -22,35 +25,39 @@ def home(request):
 
 def teachers(request):
     teachers = Teacher.objects.all()
-    return render(request, 'main/teachers.html', {'teachers': teachers})
+    news = get_news()
+    return render(request, 'main/teachers.html', {'teachers': teachers, 'news': news})
 
 # Gallery page
 
 def gallery(request):
     photos = PhotoGallery.objects.order_by('-date')
-    return render(request, 'main/gallery.html', {'photos': photos})
+    news = get_news()
+    return render(request, 'main/gallery.html', {'photos': photos, 'news': news})
 
 # News page
 
 def news_list(request):
-    news = News.objects.filter(is_active=True).order_by('-date')
+    news = get_news()
     return render(request, 'main/news_list.html', {'news': news})
 
 # Posts page
 
 def posts(request):
     posts = Post.objects.order_by('-date')
-    return render(request, 'main/posts.html', {'posts': posts})
+    news = get_news()
+    return render(request, 'main/posts.html', {'posts': posts, 'news': news})
 
 # Post detail with comments
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.order_by('-date')
+    news = get_news()
     if request.method == 'POST':
         name = request.POST.get('name')
         content = request.POST.get('content')
         if name and content:
             Comment.objects.create(post=post, name=name, content=content, date=timezone.now())
             return redirect('post_detail', pk=post.pk)
-    return render(request, 'main/post_detail.html', {'post': post, 'comments': comments})
+    return render(request, 'main/post_detail.html', {'post': post, 'comments': comments, 'news': news})
