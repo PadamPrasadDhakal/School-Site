2a116b3137efd3aa7817237e5dfce4322e55cf84
Sohan <abc@gmail.com>
2025-07-12 22:37:37 +0545

    Final push for today(saturday)

--- school_site/settings.py
+++ school_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$zmarh!%@hq72)4^_af^wd*y@e+*#ww2a2jtqz$n$a0iyz4nq+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.18.134','127.0.0.1']
 
 
 # Application definition
